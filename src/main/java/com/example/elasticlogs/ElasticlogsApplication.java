2080a9a0f89494983f8d4cb68db0a2aaa2a27d87
Alejandro Rodriguez Moreno <alejandro.rodriguezm.external@atos.net>
2021-11-30 12:02:58 +0100

    primer elastic log

---
+++ src/main/java/com/example/elasticlogs/ElasticlogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticlogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elasticlogs/ElasticlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
